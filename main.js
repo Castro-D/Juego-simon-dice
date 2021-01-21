1fd194d146a0a0a092c40971923bd1a8b091059c
Castro-D <castro.dh91@gmail.com>
2021-01-21 15:33:10 -0300

    agrega turno humano
    agrega la secuencia del humano y resalta botones clickeados

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//machine turn
+
 const $listaBotones = document.querySelectorAll('.botones');
 const $jugadaBot = $listaBotones[Math.floor(Math.random() * $listaBotones.length)];
 const secuenciaBot = [];
@@ -8,9 +8,28 @@ secuenciaBot.forEach(function(movimientoBot){
     resaltarBoton(movimientoBot)
 })
 
+const secuenciaHumano = [];
+document.querySelectorAll('.botones').forEach(function($boton){
+    $boton.onclick = manejarInputUsuario
+})
+
 function resaltarBoton(movimientoBot){
-    movimientoBot.style.filter = 'brightness(100%)';
+    movimientoBot.style.filter = 'brightness(1)';
     setTimeout(function() {
-        movimientoBot.style.filter = 'brightness(40%)';
+        movimientoBot.style.filter = 'brightness(0.4)';
         }, 500);
     }  
+
+function manejarInputUsuario(e){
+    const $boton = e.target;
+    resaltarBoton($boton);
+    secuenciaHumano.push($boton);
+    const botonMaquina = secuenciaBot[secuenciaHumano.length - 1];
+    if($boton.id !== botonMaquina.id){
+        perder();
+        }
+    }
+
+function perder(){
+    alert('Perdiste.')
+}
